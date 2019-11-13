dbe29fef9707a97dccf953d3ad7e14462c527edd
aleksandra.gjinovska <aleksandra.gjinovska@it-labs.com>
2019-11-13 08:31:47 +0100

    sql injection

---
+++ 4. Injection/before/1-Injection/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI;
+
+
+namespace _1_Injection
+{
+  public partial class _Default : Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var connString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+      var sqlString = "SELECT * FROM ProductSubcategory ORDER BY Name";
+      using (var conn = new SqlConnection(connString))
+      {
+        var command = new SqlCommand(sqlString, conn);
+        command.Connection.Open();
+        CategoryGridView.DataSource = command.ExecuteReader();
+        CategoryGridView.DataBind();
+      }
+    }
+  }
+}
---
+++ 4. Injection/before/1-Injection/Product.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1_Injection
+{
+  public partial class Product : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var productSubCategoryId = Request.QueryString["ProductSubCategoryId"];
+
+      var connString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+      var sqlString = "SELECT * FROM Product WHERE ProductSubCategoryID = @ProductSubCategoryId";
+      using (var conn = new SqlConnection(connString))
+      {
+        using (var command = new SqlCommand(sqlString, conn))
+        {
+
+         // command.CommandType = System.Data.CommandType.StoredProcedure;
+          command.Parameters.Add("@ProductSubCategoryId", System.Data.SqlDbType.VarChar).Value = productSubCategoryId;
+          command.Connection.Open();
+          ProductGridView.DataSource = command.ExecuteReader();
+          ProductGridView.DataBind();
+        }
+      }
+
+      ProductCount.Text = ProductGridView.Rows.Count.ToString("n0");
+    }
+  }
+}
